3977b6431c97db2428616e08a4588550941b62a9
shubham-gup <shubham.gupta@squadvoice.co>
2022-01-07 00:29:19 +0530

    added eslint, prettier, pre-commit hook

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to SquadStack!
-      </header>
+      <header className="App-header">Welcome to SquadStack!</header>
     </div>
   );
 }
