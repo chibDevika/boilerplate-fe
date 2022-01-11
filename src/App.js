3992c951fd199cbfc7af7eabeff11cccafffecfe
chibDevika <devika.chib@squadstack.com>
2022-01-11 15:37:51 +0530

    Removed commented code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './homepage';
 import Signin from './Signin';
 
@@ -11,21 +10,6 @@ function App() {
       <Homepage />
     </div>
   );
-  // const user = auth.currentUser;
-  // need to implement routing
-  // if (user) {
-  //   return (
-  //     <div className="App">
-  //       <Home />
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div className="App">
-  //       <Signin />
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
