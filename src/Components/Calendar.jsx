d4795f8208d8e14ad89271ad8e6cc074dc5c96c5
chibDevika <devika.chib@squadstack.com>
2022-01-26 23:19:16 +0530

    removed extra code

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -14,17 +14,6 @@ import { validateAccessToken } from './ValidateToken';
 import DateTimePickerComponent from './DateTimePicker';
 
 const localizer = momentLocalizer(moment);
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 800,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 function myCalendar() {
   const [myEvents, setMyEvents] = useState([]);
